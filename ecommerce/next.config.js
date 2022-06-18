ec18fd5bcb272b67a50c1db4003c2ca724a2b064
Chauxxx06 <freemanchx@hotmail.com>
2022-06-18 15:37:51 +0000

    ajust config git

--- ecommerce/next.config.js
+++ ecommerce/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects () {
+    return [
+      {
+        source: '/hi',
+        destination: '/holis',
+        permanent: true,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
